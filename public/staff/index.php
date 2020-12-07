8557bebf28825dfec17c0e6476aaa415dd19fd8e
Irakoze Maurice <iramaurimade@gmail.com>
2020-12-07 11:19:01 +0200

    Created staff area and css

---
+++ public/staff/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>GBI</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../stylesheets/staff.css">
+  </head>
+
+  <body>
+
+    <header>
+      <h1>Globe Bank Staff Area</h1>
+    </header>
+    <nav>
+      <ul>
+        <li> <a href="index.php">menu</a> </li>
+      </ul>
+    </nav>
+
+    <div id="content">
+
+    </div>
+
+    <footer>
+      &copy; <?php echo date('Y'); ?> Globe Bank
+    </footer>
+  </body>
+</html>
