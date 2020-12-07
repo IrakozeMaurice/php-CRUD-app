211048282b0bab7413ef6b226237d0382b416c08
Irakoze Maurice <iramaurimade@gmail.com>
2020-12-07 11:44:01 +0200

    Created reusable header and footer

---
+++ private/shared/staff_header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <title>GBI</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../stylesheets/staff.css">
+  </head>
+
+  <body>
+
+    <header>
+      <h1>Globe Bank Staff Area</h1>
+    </header>
+    <nav>
+      <ul>
+        <li> <a href="index.php">menu</a> </li>
+      </ul>
+    </nav>
--- public/staff/index.php
+++ public/staff/index.php
@@ -1,29 +1,9 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>GBI</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../stylesheets/staff.css">
-  </head>
-
-  <body>
-
-    <header>
-      <h1>Globe Bank Staff Area</h1>
-    </header>
-    <nav>
-      <ul>
-        <li> <a href="index.php">menu</a> </li>
-      </ul>
-    </nav>
+<?php include('../../private/shared/staff_header.php'); ?>
 
     <div id="content">
 
     </div>
 
-    <footer>
-      &copy; <?php echo date('Y'); ?> Globe Bank
-    </footer>
-  </body>
-</html>
+<?php include('../../private/shared/staff_footer.php'); ?>
