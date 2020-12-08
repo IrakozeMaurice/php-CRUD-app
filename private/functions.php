657fd0125ee3e2abcf93761115b601fca84ce8c6
Irakoze Maurice <iramaurimade@gmail.com>
2020-12-08 09:23:08 +0200

    Handled url encoding, default parameter values and prevent xss

--- private/functions.php
+++ private/functions.php
@@ -8,4 +8,17 @@ function url_for($script_path) {
   return WWW_ROOT . $script_path;
 }
 
+function u_enc($string){
+  return urlencode($string);
+}
+
+function r_enc($string){
+  return rawurlencode($string);
+}
+
+//always use this function when rendering data which comes dynamically to avoid xss
+function h($string){
+  return htmlspecialchars($string);
+}
+
 ?>
--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -1,5 +1,13 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php
 
-$id = $_GET['id'];
-echo "id: " . $id;
+// $id = isset($_GET['id']) ? $_GET['id'] : '1';   //PHP - < 7
+$id = $_GET['id'] ?? '1';   //PHP > 7
+echo "id: " . h($id);
+
 ?>
+
+<a href="show.php?name=<?php echo u_enc('John Doe'); ?>">Link</a><br>
+<a href="show.php?company=<?php echo u_enc('Widgets&More'); ?>">Link</a><br>
+<a href="show.php?query=<?php echo u_enc('!#*?'); ?>">Link</a><br>
