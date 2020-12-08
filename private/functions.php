10d567e8bd6c39b4689c3dcb4c1034c3bf9f858f
Irakoze Maurice <iramaurimade@gmail.com>
2020-12-08 11:03:56 +0200

    worked on pages section

--- private/functions.php
+++ private/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+//helper function for url paths
 function url_for($script_path) {
   // add the leading '/' if not present
   if($script_path[0] != '/') {
@@ -8,15 +9,17 @@ function url_for($script_path) {
   return WWW_ROOT . $script_path;
 }
 
+//function to encode url we get from urls
 function u_enc($string){
   return urlencode($string);
 }
 
+//function to encode url we get from urls
 function r_enc($string){
   return rawurlencode($string);
 }
 
-//always use this function when rendering data which comes dynamically to avoid xss
+//always use this function when rendering dynamic data to html to avoid xss
 function h($string){
   return htmlspecialchars($string);
 }
