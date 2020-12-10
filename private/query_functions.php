88532794fe96506f5b19f69f1a09bb5c9a700339
Irakoze Maurice <iramaurimade@gmail.com>
2020-12-10 09:58:32 +0200

    Added function to list pages from the database in the pages section index.php

--- private/query_functions.php
+++ private/query_functions.php
@@ -12,4 +12,16 @@
     return $result;
   }
 
+  //function to find all pages
+  function find_all_pages(){
+    global $db;
+
+    $query = "select * from pages ";
+    $query .= "order by subject_id asc, position asc";
+    // echo $query . '<br>';    //troubleshooting code
+    $result = mysqli_query($db,$query);
+    confirm_result_set($result);
+    return $result;
+  }
+
  ?>
