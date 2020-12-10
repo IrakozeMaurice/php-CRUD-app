dbaacf60364bb0c23c780c366ec51837d108abdd
Irakoze Maurice <iramaurimade@gmail.com>
2020-12-10 09:24:07 +0200

    Added error handling for database connection and queries

--- private/database.php
+++ private/database.php
@@ -4,6 +4,7 @@
   //function to connect to the database
   function db_connect(){
     $connection = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    confirm_db_connect();
     return $connection;
   }
 
@@ -11,6 +12,23 @@
   function db_disconnect($connection){
     if (isset($connection)) {
       mysqli_close($connection);
+    }
+  }
+
+  //function to check database connection error
+  function confirm_db_connect(){
+    if (mysqli_connect_errno()) {
+      $msg = "Database connection failed";
+      $msg .= mysqli_connect_error();
+      $msg .= " (" .mysqli_connect_errno() . ")";
+      exit($msg);
+    }
+  }
+
+  //function to check if query succeeded
+  function confirm_result_set($result_set){
+    if (!$result_set) {
+      exit("Database query failed.");
     }
   }
  ?>
--- private/query_functions.php
+++ private/query_functions.php
@@ -6,7 +6,9 @@
 
     $query = "select * from subjects ";
     $query .= "order by position asc";
+    // echo $query . '<br>';    //troubleshooting code
     $result = mysqli_query($db,$query);
+    confirm_result_set($result);
     return $result;
   }
 
